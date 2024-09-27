db9fad8aa20f47ae224a8f2d104442d839891e28
Kyle Biron <kyle@kyles-mbp.lan>
2024-09-27 16:32:31 -0400

    add stg medical claims etl and test

--- cms_synpufs/tests/staging/test_stg_beneficiaries_row_count.sql
+++ cms_synpufs/tests/staging/test_stg_beneficiaries_row_count.sql
@@ -1,11 +1,11 @@
 -- tests/staging/test_stg_beneficiaries_record_count.sql
 
 WITH actual_record_count AS (
-    SELECT COUNT(*) AS record_count FROM {{ ref('stg_beneficiaries') }}
+    SELECT COUNT(*) AS actual_count FROM {{ ref('stg_beneficiaries') }}
 ),
 
 expected_record_count AS (
-    WITH record_counts AS (
+    WITH unioned_counts AS (
         SELECT COUNT(*) AS record_count FROM READ_CSV_AUTO('{{ var("raw_dir") }}/DE1_0_2008_Beneficiary_Summary_File_Sample_1.csv')
         UNION ALL
         SELECT COUNT(*) AS record_count FROM READ_CSV_AUTO('{{ var("raw_dir") }}/DE1_0_2009_Beneficiary_Summary_File_Sample_1.csv')
@@ -13,7 +13,7 @@ expected_record_count AS (
         SELECT COUNT(*) AS record_count FROM READ_CSV_AUTO('{{ var("raw_dir") }}/DE1_0_2010_Beneficiary_Summary_File_Sample_1.csv')
     )
 
-    SELECT SUM(record_count) AS expected_record_count FROM record_counts
+    SELECT SUM(record_count) AS expected_count FROM unioned_counts
 )
 
 -- Check that the actual row count matches the expected row count
@@ -21,4 +21,4 @@ SELECT
     actual_record_count.actual_count,
     expected_record_count.expected_count
 FROM actual_record_count, expected_record_count
-WHERE actual_record_count.record_count != expected_record_count.expected_record_count
+WHERE actual_record_count.actual_count != expected_record_count.expected_count
